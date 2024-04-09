973342129719119573a353a645199a23d92f9b2d
Max Edwards <maxedwards20@gmail.com>
2024-04-09 07:48:53 -0600

    updates the name view

--- src/Client/Menu/ChooseNameView.cs
+++ src/Client/Menu/ChooseNameView.cs
@@ -82,7 +82,7 @@ namespace Client.Menu
                 (m_graphics.PreferredBackBufferHeight - textSize.Y) / 2);
 
             // Draw "Enter Your Name" text
-            m_spriteBatch.DrawString(font, enterNameText, textPosition, Color.PaleGoldenrod);
+            m_spriteBatch.DrawString(font, enterNameText, textPosition, Colors.displayColor);
 
             // Draw "Press Enter to proceed" below the name text if a name has been entered
             if (playerName.Length > 0)
@@ -93,7 +93,7 @@ namespace Client.Menu
                     (m_graphics.PreferredBackBufferWidth - proceedTextSize.X) / 2,
                     textPosition.Y + textSize.Y + 20); // 20 pixels below the name text
 
-                m_spriteBatch.DrawString(font, proceedText, proceedTextPosition, Color.PaleGoldenrod);
+                m_spriteBatch.DrawString(font, proceedText, proceedTextPosition, Colors.displayColor);
             }
 
             m_spriteBatch.End();
